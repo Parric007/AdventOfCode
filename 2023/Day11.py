6453a39909060c28c1abbed9c0380bc2915debf8
Parric007 <liam.huppert@gmx.de>
2023-12-20 10:46:16 +0100

    Update Day11.py

--- 2023/Day11.py
+++ 2023/Day11.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 ll = [x for x in open("Day11Input.txt").read().strip().split("\n\n")][0]
 
 matrix = ll.split("\n")
@@ -7,8 +9,8 @@ for i in range(len(matrix)):
 
 emptyRows = []
 emptyCols = []
-
-print(matrix)
+positionsGalaxies = []
+adjustedPos = []
 
 for i in range(len(matrix)):
     if "#" not in matrix[i]:
@@ -18,11 +20,33 @@ for i in range(len(matrix[0])):
     colIsEmpty = True
     for j in range(len(matrix)):
         if matrix[j][i] == "#":
+            positionsGalaxies.append((j,i))
             colIsEmpty = False
             break
     if colIsEmpty:
         emptyCols.append(i)
 
+for cols in emptyCols:
+    for pos in positionsGalaxies:
+        if pos[1] > cols:
+            adjustedPos.append((pos[0], pos[1]+1))
+        else:
+            adjustedPos.append(pos)
 
-print(emptyCols)
-print(emptyRows)
+for rows in emptyRows:
+    for pos in positionsGalaxies:
+        if pos[0] > rows:
+            adjustedPos.append((pos[0]+1, pos[1]))
+        else:
+            adjustedPos.append(pos)
+
+result1 = 0
+
+#res = [(a, b) for idx, a in enumerate(adjustedPos) for b in adjustedPos[idx + 1:]]
+res = list(combinations(adjustedPos, 2))
+
+for val in res:
+    result1 += abs(val[0][0]-val[1][0]) + abs(val[0][1]-val[1][1])
+
+print(result1)
+#9 648 398
