2471379c0c5e9fd2b54abc890fd853ddf8a64f57
Parric <liam.huppert@gmx.de>
2024-01-03 13:15:40 +0100

    Update Day12.py

--- 2023/Day12.py
+++ 2023/Day12.py
@@ -1,7 +1,19 @@
 #7843 A1
 #10153896718999 A2
 
+def getPossibleArrangements(part, numberOfParts):
+    if "#" not in part:
+        return len(part) - numberOfParts + 1
+    return 0
+
+def checkEmpty(arrIn):
+    if arrIn == "":
+        return False
+    return True
+
+
 ll = [x for x in open("Day12Input.txt").read().strip().split("\n\n")]
+
 
 matrix = ll[0].split("\n")
 puzzleInput = []
@@ -12,5 +24,15 @@ for lin in matrix:
     counts = list(map(int, counts))
     puzzleInput.append((split[0], counts))
 
-print(puzzleInput)
+result1 = 0
+for puzzles in puzzleInput:
+    toCheck = puzzles[0].split(".")
+    iterator = filter(checkEmpty, toCheck)
+    toCheck = list(iterator)
+    print(toCheck)
+    print(puzzles[1])
+    for i in range(len(toCheck)):
+        arr = toCheck[i]
+        result1 += getPossibleArrangements(arr, puzzles[1][i])
 
+print(result1)
