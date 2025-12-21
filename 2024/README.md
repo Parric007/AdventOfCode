23f78ac450a2dad714ea62f8a92d4b65f5f5dc40
Parric <liam.huppert@gmx.de>
2025-12-21 12:28:31 +0100

    2024 Day 10

--- 2024/README.md
+++ 2024/README.md
@@ -4,94 +4,104 @@ By Liam
 ### Part one
 
 Result: 1506483 \
-Time: 519.142us
+Time: 557.478us
 ### Part two
 
 Result: 23126924 \
-Time: 362.273us
+Time: 364.851us
 
 ## Day 2
 ### Part one
 
 Result: 663 \
-Time: 248.839us
+Time: 304.351us
 ### Part two
 
 Result: 692 \
-Time: 301.005us
+Time: 297.866us
 
 ## Day 3
 ### Part one
 
 Result: 188192787 \
-Time: 219.622us
+Time: 260.802us
 ### Part two
 
 Result: 113965544 \
-Time: 344.181us
+Time: 330.046us
 
 ## Day 4
 ### Part one
 
 Result: 2468 \
-Time: 268.781us
+Time: 262.849us
 ### Part two
 
 Result: 1864 \
-Time: 345.339us
+Time: 340.39us
 
 ## Day 5
 ### Part one
 
 Result: 4569 \
-Time: 533.476us
+Time: 519.104us
 ### Part two
 
 Result: 6456 \
-Time: 1.916478ms
+Time: 1.760864ms
 
 ## Day 6
 ### Part one
 
 Result: 5404 \
-Time: 130.529us
+Time: 121.789us
 ### Part two
 
 Result: 1984 \
-Time: 395.134638ms
+Time: 384.948058ms
 
 ## Day 7
 ### Part one
 
 Result: 538191549061 \
-Time: 792.603us
+Time: 2.629898ms
 ### Part two
 
 Result: 34612812972206 \
-Time: 23.690055ms
+Time: 94.232064ms
 
 ## Day 8
 ### Part one
 
 Result: 400 \
-Time: 86.372us
+Time: 80.191us
 ### Part two
 
 Result: 1280 \
-Time: 76.195us
+Time: 91.689us
 
 ## Day 9
 ### Part one
 
 Result: 6154342787400 \
-Time: 824.067us
+Time: 863.946us
 ### Part two
 
 Result: 6183632723350 \
-Time: 65.411929ms
+Time: 67.123124ms
+
+## Day 10
+### Part one
+
+Result: 638 \
+Time: 154.416us
+### Part two
+
+Result: 1289 \
+Time: 123.658us
 
 Total time:
-491.205524ms \
+555.367434ms \
 Each run was averaged over 1000 runs
 
 For Josy. Forever
---
+++ 2024/src/main/kotlin/Day10.kt
@@ -0,0 +1,63 @@
+package main
+
+import java.io.File
+
+class Day10: Day {
+    override fun processTextInputPartOne(filePath: String): Long {
+        val (startingPos, grid) = getGridAndPositions(filePath)
+        return startingPos.sumOf { explorePaths(grid, it, mutableSetOf()) }
+    }
+
+    override fun processTextInputPartTwo(filePath: String): Long {
+        val (startingPos, grid) = getGridAndPositions(filePath)
+        return startingPos.sumOf { explorePaths(grid, it, null) }
+    }
+
+    private fun getGridAndPositions(filePath: String): Pair<MutableSet<Point>, List<List<Int>>> {
+        val startingPos = mutableSetOf<Point>()
+        val grid = File(filePath).readLines().mapIndexed { y, str -> str.mapIndexed { x, c ->
+            if (c == '0') startingPos.add(Point(x, y))
+            c.digitToInt()
+        }}
+        return startingPos to grid
+    }
+
+    private fun explorePaths(grid: List<List<Int>>, currentPosition: Point, alreadyReachedNines: MutableSet<Point>?): Long {
+        val currentHeight = grid[currentPosition.y][currentPosition.x]
+        if (currentHeight == 9) {
+            if (alreadyReachedNines == null) return 1
+            if (alreadyReachedNines.contains(currentPosition)) return 0
+            alreadyReachedNines.add(currentPosition)
+            return 1
+        }
+
+        var result = 0L
+        if (currentPosition.y > 0) {
+            val up = grid[currentPosition.y - 1][currentPosition.x]
+            if (up == currentHeight + 1) {
+                result += explorePaths(grid, Point(currentPosition.x, currentPosition.y - 1), alreadyReachedNines)
+            }
+        }
+        if (currentPosition.y < grid.lastIndex) {
+            val down = grid[currentPosition.y + 1][currentPosition.x]
+            if (down == currentHeight + 1) {
+                result += explorePaths(grid, Point(currentPosition.x, currentPosition.y + 1), alreadyReachedNines)
+            }
+        }
+        if (currentPosition.x > 0) {
+            val left = grid[currentPosition.y][currentPosition.x - 1]
+            if (left == currentHeight + 1) {
+                result += explorePaths(grid, Point(currentPosition.x - 1, currentPosition.y), alreadyReachedNines)
+            }
+        }
+        if (currentPosition.x < grid[0].lastIndex) {
+            val right = grid[currentPosition.y][currentPosition.x + 1]
+            if (right == currentHeight + 1) {
+                result += explorePaths(grid, Point(currentPosition.x + 1, currentPosition.y), alreadyReachedNines)
+            }
+        }
+        return result
+    }
+
+    data class Point(val x: Int, val y: Int)
+}
